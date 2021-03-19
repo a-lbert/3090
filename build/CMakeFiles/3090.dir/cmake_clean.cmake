a28241fb7449bf6d6120bbc8aa479a0e73ea7fd6
a-lbert <1049381021@qq.com>
2021-03-19 21:24:31 +0800

    编译通过

---
+++ build/CMakeFiles/3090.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3090.dir/main.cpp.o"
+  "CMakeFiles/3090.dir/in.cpp.o"
+  "CMakeFiles/3090.dir/CodeCraft.cpp.o"
+  "3090.pdb"
+  "3090"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3090.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sz2/HuaWei/3090
+# Build directory: /home/sz2/HuaWei/3090/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
